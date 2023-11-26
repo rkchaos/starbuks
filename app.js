0ad24fd50eb556a9ee4ffc04a7b6f1240ddc44f5
Karan Bisht <134302942+Karan-Bisht16@users.noreply.github.com>
2023-11-26 13:11:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 function imgslider(item){
     document.querySelector('.image')=item;
-    }
-  
+}
+const mainImage = document.querySelector('#mainImage');
+function changeMainImage(value){
+    mainImage.setAttribute('src', "./assests/img"+value+".png")
+} 
