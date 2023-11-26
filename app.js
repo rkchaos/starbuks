268cd1593df742cd27252464ac9e2aa032c00426
Karan Bisht <134302942+Karan-Bisht16@users.noreply.github.com>
2023-11-26 14:08:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ function imgslider(item){
     document.querySelector('.image')=item;
 }
 const mainImage = document.querySelector('#mainImage');
+const rightCircle = document.querySelector('.right_circle');
+const colorArray = ['#005b35', '#e6bbbd', '#a2266d']
 function changeMainImage(value){
     mainImage.setAttribute('src', "./assests/img"+value+".png")
+    rightCircle.style.backgroundColor= colorArray[value-1];
 } 
